391d851bdaddda52af548b8f49557bcf3319121b
Julian Sapundzhiev <julian_s@abv.bg>
2018-06-11 12:28:16 +0300

    SPI reading and detection

---
+++ PiPiano/ItemState.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiPiano
+{
+    /// <summary>
+    /// Class to store item state
+    /// </summary>
+    public class ItemState
+    {
+        private const int MinPressedValue = 140;
+        private const int ValueStackSize = 3;
+
+        public bool IsPressed { get; set; }
+
+        public List<int> LastValues { get; set; }
+
+        public bool UpdateState(int value)
+        {
+            UpdateStack(value);
+            bool newState = CalculatePressed();
+            IsPressed = newState;
+            return newState;
+        }
+
+        private void UpdateStack(int value)
+        {
+            if (LastValues == null)
+                LastValues = new List<int>();
+
+            LastValues.Add(value);
+
+            if (LastValues.Count > ValueStackSize)
+                LastValues.RemoveAt(0);
+        }
+
+        private bool CalculatePressed()
+        {
+            foreach (var value in LastValues)
+                if (value <= MinPressedValue)
+                    return false;
+            return LastValues.Count >= ValueStackSize;
+        }
+    }
+}
--- PiPiano/MainPage.xaml.cs
+++ PiPiano/MainPage.xaml.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
 using Windows.Devices.Gpio;
+using Windows.Devices.Spi;
 using Windows.Media.Core;
 using Windows.Media.Playback;
 using Windows.UI.Xaml;
@@ -14,85 +20,179 @@ namespace PiPiano
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private const int NUM_OF_SOUNDS = 12;
-        private static readonly int[] PB_PIN_NUMBERS = new int[] { 2,
-
-3,
-
-4,
-
-5,
-
-6,
-
-7,
-
-8,
-
-9,
-
-10,
-
-11,
-
-12,
-
-13
-};
-        private GpioPin[] pushButtons = new GpioPin[NUM_OF_SOUNDS];
-        private bool[] downStates = new bool[NUM_OF_SOUNDS];
         private DispatcherTimer timer;
-        private MediaPlayer player = new MediaPlayer();
 
+        private const int NUM_OF_SOUNDS = 12;
+        private ItemState[] downStates = new ItemState[NUM_OF_SOUNDS];
+        private MediaPlayer[] players = new MediaPlayer[NUM_OF_SOUNDS];
 
+        private SpiDevice ADC;
+        private SpiDevice ADC2;
 
         public MainPage()
         {
             this.InitializeComponent();
 
+            InitPlayers();
+
             var gpio = GpioController.GetDefault();
             if (gpio != null)
             {
-                InitGPIO(gpio);
                 timer = new DispatcherTimer();
-                timer.Interval = TimeSpan.FromMilliseconds(500);
+                timer.Interval = TimeSpan.FromMilliseconds(1000);
                 timer.Tick += Timer_Tick;
                 timer.Start();
             }
         }
 
-        private void InitGPIO(GpioController gpio)
+        private void InitPlayers()
         {
-            for (int i = 0; i < PB_PIN_NUMBERS.Length; i++)
+            for (int i = 0; i < NUM_OF_SOUNDS; i++)
             {
-                Console.WriteLine("Pin-" + PB_PIN_NUMBERS[i]);
-                pushButtons[i] = gpio.OpenPin(PB_PIN_NUMBERS[i]);
-                pushButtons[i].SetDriveMode(GpioPinDriveMode.Input);
-                downStates[i] = false;
+                players[i] = new MediaPlayer();
+                players[i].Source = MediaSource.CreateFromUri(new Uri($"ms-appx:///Assets/sound{i}.mp3"));
+                downStates[i] = new ItemState();
             }
         }
 
-        private void Timer_Tick(object sender, object e)
+        private async Task InitSpi()
         {
-            CheckButtonState();
+            if (ADC == null)
+            {
+                ADC = await InitSPI(0);
+            }
+
+            if (ADC2 == null)
+            {
+                ADC2 = await InitSPI(1);
+                timer.Interval = TimeSpan.FromMilliseconds(200);
+            }
         }
 
-        private void CheckButtonState()
+        private async Task<SpiDevice> InitSPI(int busNimber)
         {
-            for (int i = 0; i < pushButtons.Length; i++)
+            var settings = new SpiConnectionSettings(0)                         // Chip Select line
             {
-                bool isPinOn = pushButtons[i].Read() == GpioPinValue.High;
-                if (downStates[i] != isPinOn && isPinOn)
+                ClockFrequency = 3600000,                                    // Don't exceed 3.6 MHz
+                Mode = SpiMode.Mode0,
+            };
+
+            string spiAqs = SpiDevice.GetDeviceSelector(); // ($"SPI{busNimber}");                /* Find the selector string for the SPI bus controller          */
+            var devicesInfo = await DeviceInformation.FindAllAsync(spiAqs);     /* Find the SPI bus controller device with our selector string  */
+            SpiDevice spiDevice = await SpiDevice.FromIdAsync(devicesInfo[busNimber].Id, settings);     /* Create an SpiDevice with our bus controller and SPI settings */
+            return spiDevice;
+        }
+
+        private async void Timer_Tick(object sender, object e)
+        {
+            await InitSpi();
+
+            ReadSpiData();
+        }
+
+        private void ReadSpiData()
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                int value = ReadAdc(i, ADC);
+                Trace.Write(i + " = [" + value + " ], ");
+
+                ItemState state = downStates[i];
+                bool wasPressed = state.IsPressed;
+                
+                if (state.UpdateState(value) && !wasPressed)
+                {
                     playNote(i);
-                downStates[i] = isPinOn;
+                }
             }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int value = ReadAdc(i, ADC2);
+                Trace.Write(i + 8 + " = [" + value + " ], ");
+
+                ItemState state = downStates[i + 8];
+                bool wasPressed = state.IsPressed;
+
+                if (state.UpdateState(value) && !wasPressed)
+                {
+                    playNote(i + 8);
+                }
+            }
+
+            Trace.WriteLine("");
+        }
+
+        private static int ReadAdc(int adc_number, SpiDevice spiADC)
+        {
+            byte byte1 = (byte)1;
+            byte byte2 = GetRequestChannelByte(adc_number);
+
+            byte[] request = new byte[3] { byte1, byte2, 0x0 };
+            byte[] response = new byte[3];
+
+            spiADC.TransferFullDuplex(request, response);
+
+            int value = ConverToInt(response);
+            return value;
+        }
+
+        private static byte GetRequestChannelByte(int adc_number)
+        {
+            //(byte)(0b10000000 | ((adc_number & 7) << 4));
+
+            byte byte2 = 0b0000000;
+
+            switch (adc_number)
+            {
+                case 0:
+                    byte2 = 0b0000000;
+                    break;
+                case 1:
+                    byte2 = 0b0010000;
+                    break;
+                case 2:
+                    byte2 = 0b0100000;
+                    break;
+                case 3:
+                    byte2 = 0b0110000;
+                    break;
+                case 4:
+                    byte2 = 0b1000000;
+                    break;
+                case 5:
+                    byte2 = 0b1010000;
+                    break;
+                case 6:
+                    byte2 = 0b1100000;
+                    break;
+                case 7:
+                    byte2 = 0b1110000;
+                    break;
+            }
+
+            return byte2;
+        }
+
+        private static int ConverToInt(byte[] data)
+        {
+            int result = 0;
+            result = data[1] & 0x03;
+            result <<= 8;
+            result += data[2];
+            return result;
         }
 
         private void playNote(int i)
         {
-            MediaPlayer mediaPlayer = new MediaPlayer();
-            mediaPlayer.Source = MediaSource.CreateFromUri(new Uri($"ms-appx:///Assets/sound{i}.wav"));
-            mediaPlayer.Play();
+            if(players[i].PlaybackSession.CanPause)
+            {
+                players[i].Dispose();
+                players[i] = new MediaPlayer();
+                players[i].Source = MediaSource.CreateFromUri(new Uri($"ms-appx:///Assets/sound{i}.mp3"));
+            }
+
+            players[i].Play();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
